73897ac9d6982b086c036c39fce1e9283de622fe
Scott Peterson <scott@headway.io>
2022-12-01 14:07:56 -0500

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,46 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val testInput = readInput("Day01_test")
+        var map: MutableMap<Int, Int> = mutableMapOf()
+        var key = 1
+
+        testInput.forEach {
+            if (it == "") {
+                ++key
+                return@forEach
+            }
+
+            map[key] = map[key]?.plus(it.toInt()) ?: 0.plus(it.toInt())
+        }
+        return map.values.maxOrNull()!!
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val testInput = readInput("Day01_test")
+        var map: MutableMap<Int, Int> = mutableMapOf()
+        var key = 1
+
+        testInput.forEach {
+            if (it == "") {
+                ++key
+                return@forEach
+            }
+
+            map[key] = map[key]?.plus(it.toInt()) ?: 0.plus(it.toInt())
+        }
+
+        val sortedValues = map.toList().sortedByDescending { (key, value) -> value }.toMap().values
+
+        print(sortedValues.take(3).sum())
+
+        return sortedValues.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 72240)
+//
+    val input = readInput("Day01_test")
     println(part1(input))
     println(part2(input))
 }
